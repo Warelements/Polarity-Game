432226a3882f6cbd2673f7d66af6516eb21b2ede
Mohammed ursalaan latif <ursalaan1@gmail.com>
2019-03-31 22:09:26 +0100

    typewriter text update v1
    typewriter text updated but does not function for interactable objects yet.

--- Assets/Azlan Parts/MU_Scripts/MU_TypewriterText.cs
+++ Assets/Azlan Parts/MU_Scripts/MU_TypewriterText.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class MU_TypewriterText : MonoBehaviour
 {
     public TextMesh Tm_Text;
+    public Text Globaltext;
     string Story;
     string StartText;
     bool playtext;
     public float TextSpeed;
+    public HL_Text_Trigger triggerscript;
     void Awake()
     {
+        
         Tm_Text = GetComponent<TextMesh>();
         if(Tm_Text==null)
         {
@@ -21,23 +24,67 @@ public class MU_TypewriterText : MonoBehaviour
         StartText = Story;
         // TODO: add optional delay when to start     
     }
-    private void OnEnable()
+    private void Update()
     {
-        //print("Enable");
-        Tm_Text.text = "";
-        StartCoroutine("PlayText");
+        if (triggerscript != null)
+        {
+            if (triggerscript.Bl_Enabled)
+            {
+                GetComponent<MeshRenderer>().enabled = false;
+                if (Globaltext != null)
+                {
+                    Globaltext.text = "";
 
-}
+                    StartCoroutine("PlayText");
+                }
+            }
+            //if (!triggerscript.Bl_Enabled)
+            //{
+            //    print("hnn");
+            //    Globaltext.text = "";
+            //    Tm_Text.text = StartText;
+            //    print("huh");
+
+            //}
+        }
+    }
+    //    private void OnEnable()
+    //    {
+    //        //print("Enable");
+    //        //global text.text == blank
+    //        //if (triggerscript!=null)
+    //        //{
+    //        //    if (triggerscript.Bl_Enabled)
+    //        //    {
+
+    //        //    } 
+    //        //}
+
+
+    //        if (Globaltext != null)
+    //        {
+    //            Globaltext.text = "";
+
+    //            StartCoroutine("PlayText");
+    //        }
+    //}
     private void OnDisable()
     {
+        print("hnn");
+        if (Globaltext!=null)
+        {
+            Globaltext.text = ""; 
+        }
         Tm_Text.text = StartText;
-       // Debug.Log("PrintOnDisable: script was disabled");
+        print("huh");
+        //Tm_Text.text = StartText;
+        // Debug.Log("PrintOnDisable: script was disabled");
     }
     IEnumerator PlayText()
     {
         foreach (char c in Story)
         {
-            Tm_Text.text += c;
+            Globaltext.text+= c;
             yield return new WaitForSeconds(TextSpeed);
         }
     }
--- Assets/Scripts/Interactions/In Game/HL_Text_Trigger.cs
+++ Assets/Scripts/Interactions/In Game/HL_Text_Trigger.cs
@@ -6,10 +6,12 @@ public class HL_Text_Trigger : MonoBehaviour {
 
     protected GameObject MyText;
     protected GameObject PC;
+    public bool Bl_Enabled;
 	// Use this for initialization
 	void Start () {
         MyText = gameObject.transform.Find("Text Holder").gameObject;
         MyText.SetActive(false);
+        Bl_Enabled = false;
         PC = GameObject.FindGameObjectWithTag("Player");
 	}
 	
@@ -23,14 +25,17 @@ public class HL_Text_Trigger : MonoBehaviour {
         {
             HL_Joystick.instance.SwichInTrigger(true);
             MyText.SetActive(true);
+            Bl_Enabled = true;
 
         }
         if (collision.tag == "Player" && gameObject.tag == "Interactable")
         {
             print("i have collided");
+            Bl_Enabled = true;
             HL_Joystick.instance.SwichInTrigger(true);
             HL_PC.instance.SetString("Interact");
             HL_PC.instance.SetTextBox(gameObject);
+
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -38,7 +43,9 @@ public class HL_Text_Trigger : MonoBehaviour {
         if (collision.tag == "Player")
         {
             HL_Joystick.instance.SwichInTrigger(false);
+            Bl_Enabled = false;
             MyText.SetActive(false);
+            
             HL_PC.instance.SetTextBox(null);
             HL_PC.instance.SetString("Jump");
         }
@@ -46,5 +53,6 @@ public class HL_Text_Trigger : MonoBehaviour {
     public void TriggerInteractableBox()
     {
         MyText.SetActive(true);
+        Bl_Enabled = true;
     }
 }
